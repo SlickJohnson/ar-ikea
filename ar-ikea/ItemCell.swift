73d235ff9bf978bf416eafdca18cd03afe767e6a
SlickJohnson <liwa.johnson@gmail.com>
2018-10-07 14:29:28 -0700

    Add items to collection view

---
+++ ar-ikea/ItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCell.swift
+//  ar-ikea
+//
+//  Created by Willie Johnson on 10/7/18.
+//  Copyright © 2018 Willie Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UICollectionViewCell {
+    
+  @IBOutlet weak var itemLabel: UILabel!
+}
--- ar-ikea/ViewController.swift
+++ ar-ikea/ViewController.swift
@@ -15,10 +15,39 @@ class ViewController: UIViewController {
 
   let configuration = ARWorldTrackingConfiguration()
 
+  let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
   override func viewDidLoad() {
     super.viewDidLoad()
     sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
     sceneView.session.run(configuration)
+    itemsCollectionView.dataSource = self
+    itemsCollectionView.delegate = self
+  }
+}
+
+/// MARK: - UICollectionViewDataSource
+extension ViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return itemsArray.count
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! ItemCell
+    cell.itemLabel.text = itemsArray[indexPath.row]
+    return cell
+  }
+}
+
+/// MARK: - UICollectionViewDelegate
+extension ViewController: UICollectionViewDelegate {
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    let cell = collectionView.cellForItem(at: indexPath)
+    cell?.backgroundColor = UIColor.green
+  }
+
+  func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+    let cell = collectionView.cellForItem(at: indexPath)
+    cell?.backgroundColor = UIColor.orange
   }
 }
 
