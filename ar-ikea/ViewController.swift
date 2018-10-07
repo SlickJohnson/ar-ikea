14496990fbd06d5e12dcb596954b8dbe71824e9f
SlickJohnson <liwa.johnson@gmail.com>
2018-10-07 15:25:10 -0700

    Add missing files from last commit

--- ar-ikea/ViewController.swift
+++ ar-ikea/ViewController.swift
@@ -15,13 +15,57 @@ class ViewController: UIViewController {
 
   let configuration = ARWorldTrackingConfiguration()
 
+  /// Addable furniture items.
   let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
+
+  /// The item that was selected in the collection view.
+  var selectedItem: String?
+
   override func viewDidLoad() {
     super.viewDidLoad()
     sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+    configuration.planeDetection = .horizontal
     sceneView.session.run(configuration)
     itemsCollectionView.dataSource = self
     itemsCollectionView.delegate = self
+    registerGestureRecognizers()
+  }
+}
+
+/// MARK: Helper
+private extension ViewController {
+  /// Adds tap gesture recognizer to scene view.
+  func registerGestureRecognizers() {
+    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+    sceneView.addGestureRecognizer(tapGestureRecognizer)
+  }
+
+  /// Handles taps on scene view.
+  @objc func tapped(sender: UITapGestureRecognizer) {
+    let sceneView = sender.view as! ARSCNView
+    let tapLocation = sender.location(in: sceneView)
+    let hitTest = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+    if !hitTest.isEmpty {
+      addItem(hitTestResult: hitTest.first!)
+    }
+  }
+
+  /// Adds the item item to detected plan based on item tapped in collection view.
+  func addItem(hitTestResult: ARHitTestResult) {
+    // Check if there's an item selected.
+    guard let selectedItem = selectedItem else { return }
+
+    // Grab item node from scene with same name.
+    guard let scene = SCNScene(named: "Models.scnassets/\(selectedItem).scn") else { return }
+    guard let node = scene.rootNode.childNode(withName: selectedItem, recursively: false) else { return }
+
+    // Grab position information of detected plane.
+    let transform = hitTestResult.worldTransform
+    let thirdColumn = transform.columns.3
+
+    // Position item at detected plane.
+    node.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
+    sceneView.scene.rootNode.addChildNode(node)
   }
 }
 
@@ -43,6 +87,7 @@ extension ViewController: UICollectionViewDelegate {
   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     let cell = collectionView.cellForItem(at: indexPath)
     cell?.backgroundColor = UIColor.green
+    selectedItem = itemsArray[indexPath.row]
   }
 
   func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
